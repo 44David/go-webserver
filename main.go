430e75239e122df36e19a8907ef2ab8650ee7ff8
David Soane <david.soane@outlook.com>
2024-06-02 18:53:22 -0600

    set up root directory and web server logic

--- main.go
+++ main.go
@@ -9,7 +9,27 @@ import (
 
 )
 
-func root(write http.ResponseWriter, req *http.Request) {
-	fmt.Printf("Root request recieved")
-	io.WriteString(write, "Write given to server" )
+func dirRoot(write http.ResponseWriter, req *http.Request) {
+	fmt.Printf("/root directory request received\n")
+	io.WriteString(write, "Content served to the browser from root directory\n" )
+}
+
+func dirTest(write http.ResponseWriter, req *http.Request) {
+	fmt.Printf("/test directory request received\n")
+	io.WriteString(write, "Content served at route /hello\n" )
+}
+
+func main() {
+	http.HandleFunc("/", dirRoot)
+	http.HandleFunc("/test", dirTest)
+	
+	// blocking call (like async await in javacript)
+	err := http.ListenAndServe(":3000", nil)
+
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("Server was closed\n")
+	} else if err != nil {
+		fmt.Printf("Error occured when starting server %s\n", err)
+		os.Exit(1)
+	}
 }
